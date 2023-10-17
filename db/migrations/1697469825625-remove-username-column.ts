30bbc12281fbc7242f7622f05e3a07fe0c680a0a
kishor82 <kishorrathva8298@gmail.com>
2023-10-17 14:22:36 +0530

    update: formated migrations.

--- db/migrations/1697469825625-remove-username-column.ts
+++ db/migrations/1697469825625-remove-username-column.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class RemoveUsernameColumn1697469825625 implements MigrationInterface {
-    name = 'RemoveUsernameColumn1697469825625'
+  name = 'RemoveUsernameColumn1697469825625';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "username"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "username"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "username" character varying NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "username" character varying NOT NULL`,
+    );
+  }
 }
