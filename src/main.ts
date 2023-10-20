62c7a66f2e983f800a485436e15bf108a1f85202
kishor82 <kishorrathva8298@gmail.com>
2023-10-20 20:20:57 +0530

    Implemented redis store with express session.

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,53 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import RedisStore from 'connect-redis';
+import { default as Redis } from 'ioredis';
 import * as session from 'express-session';
 import * as passport from 'passport';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+class Main {
+  async bootstrap() {
+    const logger = new Logger(Main.name);
+    const redisStore = new RedisStore({
+      client: Redis,
+      prefix: 'myapp:',
+    });
+    const port = 3000;
 
-  app.use(
-    session({
-      secret: 'keyboard cat', // TODO : get it from env
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 360000,
-      },
-    }),
-  );
+    const app = await NestFactory.create(AppModule);
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+    app.use(
+      session({
+        store: redisStore,
+        secret: 'keyboard cat', // TODO : get it from env
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+          maxAge: 360000,
+        },
+      }),
+    );
 
-  app.useGlobalPipes(new ValidationPipe());
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-  const config = new DocumentBuilder()
-    .setTitle('Nest API')
-    .setDescription('the description of the API')
-    .setVersion('1.0')
-    .build();
+    app.useGlobalPipes(new ValidationPipe());
 
-  const document = SwaggerModule.createDocument(app, config);
+    const config = new DocumentBuilder()
+      .setTitle('Nest API')
+      .setDescription('the description of the API')
+      .setVersion('1.0')
+      .build();
 
-  SwaggerModule.setup('/doc', app, document);
+    const document = SwaggerModule.createDocument(app, config);
 
-  await app.listen(3000);
+    SwaggerModule.setup('/doc', app, document);
+
+    logger.log(`Listening on port ${port}`);
+    await app.listen(3000);
+  }
 }
-bootstrap();
+
+new Main().bootstrap();
